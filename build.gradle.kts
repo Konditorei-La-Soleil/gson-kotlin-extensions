31ad611a734f67a8b35250dc7ee141337e058952
木葉 Scarlet <93977077+mukjepscarlet@users.noreply.github.com>
2025-06-23 17:29:17 +0800

    add Netty ByteBuf extensions

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,10 +15,13 @@ dependencies {
     compileOnly("com.google.code.gson:gson:2.8.9")
     // https://mvnrepository.com/artifact/com.squareup.okio/okio
     compileOnly("com.squareup.okhttp3:okhttp:4.12.0")
+    // https://mvnrepository.com/artifact/io.netty/netty-buffer
+    compileOnly("io.netty:netty-buffer:4.1.122.Final")
 
     testImplementation(kotlin("test"))
     testImplementation("com.google.code.gson:gson:2.8.9")
     testImplementation("com.squareup.okhttp3:okhttp:4.12.0")
+    testImplementation("io.netty:netty-buffer:4.1.122.Final")
 }
 
 publishing {
---
+++ src/main/kotlin/moe/lasoleil/gson/external/Netty.kt
@@ -0,0 +1,34 @@
+@file:Suppress("NOTHING_TO_INLINE")
+package moe.lasoleil.gson.external
+
+import com.google.gson.Gson
+import com.google.gson.JsonElement
+import io.netty.buffer.ByteBuf
+import io.netty.buffer.ByteBufInputStream
+import io.netty.buffer.ByteBufOutputStream
+import moe.lasoleil.gson.parser.fromJson
+import moe.lasoleil.gson.parser.parseJson
+import moe.lasoleil.gson.parser.writeJson
+import moe.lasoleil.gson.util.DEFAULT_GSON
+import java.nio.charset.Charset
+
+inline fun ByteBuf.inputStream(): ByteBufInputStream =
+    ByteBufInputStream(this)
+
+inline fun ByteBuf.outputStream(): ByteBufOutputStream =
+    ByteBufOutputStream(this)
+
+inline fun <reified T> Gson.fromJson(source: ByteBuf, charset: Charset = Charsets.UTF_8): T =
+    fromJson(source.inputStream().reader(charset))
+
+inline fun <reified T> ByteBuf.parseJson(charset: Charset = Charsets.UTF_8, gson: Gson = DEFAULT_GSON): T =
+    gson.fromJson(this, charset)
+
+inline fun ByteBuf.parseJson(charset: Charset = Charsets.UTF_8): JsonElement =
+    inputStream().reader(charset).parseJson()
+
+inline fun ByteBuf.writeJson(jsonElement: JsonElement?, charset: Charset = Charsets.UTF_8, gson: Gson = DEFAULT_GSON) =
+    outputStream().writer(charset).writeJson(jsonElement, gson)
+
+inline fun <reified T> ByteBuf.writeJson(value: T, charset: Charset = Charsets.UTF_8, gson: Gson = DEFAULT_GSON) =
+    outputStream().writer(charset).writeJson(value, gson)
